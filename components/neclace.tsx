dea0d2305db74d657dff6a0619adf2e5ad3c95ad
mmeTanya <mme.tanyalegois@gamail.com>
2023-09-17 22:11:07 +0100

    1

---
+++ components/neclace.tsx
@@ -0,0 +1,12 @@
+import s from "../styles/clouds.module.scss";
+
+const Neclace = () => {
+
+
+  return (
+    < section className={s.s} ></section>
+
+  );
+};
+
+export default Neclace;
---
+++ components/neclace1.tsx
@@ -0,0 +1,66 @@
+import s from "../styles/clouds.module.scss";
+
+const Neclace1 = () => {
+
+
+  return (
+    <section className={s.sky} >
+      <button className={s.cloud_1}> </button>
+      <button className={s.cloud_2}> </button>
+      <button className={s.cloud_3}> </button>
+      <button className={s.cloud_4}> </button>
+      <button className={s.cloud_5}> </button>
+      <button className={s.cloud_6}></button>
+      <button className={s.cloud_7}></button>
+      <button className={s.cloud_8}> </button>
+      <button className={s.cloud_9}></button>
+      <button className={s.cloud_10}></button>
+      <button className={s.cloud_11}> </button>
+      <button className={s.cloud_12}></button>
+      <button className={s.cloud_13}> </button>
+      <button className={s.cloud_14}> </button>
+      <button className={s.cloud_15}> </button>
+      <button className={s.cloud_16}> </button>
+      <button className={s.cloud_17}> </button>
+      <button className={s.cloud_18}></button>
+      <button className={s.cloud_19}></button>
+      <button className={s.cloud_20}> </button>
+      <button className={s.cloud_21}></button>
+      <button className={s.cloud_22}></button>
+      <button className={s.cloud_23}> </button>
+      <button className={s.cloud_24}></button>
+      <button className={s.cloud_25}> </button>
+      <button className={s.cloud_26}> </button>
+      <button className={s.cloud_27}> </button>
+      <button className={s.cloud_28}></button>
+      <button className={s.cloud_29}></button>
+      <button className={s.cloud_30}> </button>
+      <button className={s.cloud_31}> </button>
+      <button className={s.cloud_32}> </button>
+      <button className={s.cloud_33}> </button>
+      <button className={s.cloud_34}> </button>
+      <button className={s.cloud_35}> </button>
+      <button className={s.cloud_36}></button>
+      <button className={s.cloud_37}></button>
+      <button className={s.cloud_38}> </button>
+      <button className={s.cloud_39}></button>
+      <button className={s.cloud_40}></button>
+      <button className={s.cloud_41}> </button>
+      <button className={s.cloud_42}></button>
+      <button className={s.cloud_43}> </button>
+      <button className={s.cloud_44}> </button>
+      <button className={s.cloud_45}> </button>
+      <button className={s.cloud_46}> </button>
+      <button className={s.cloud_47}> </button>
+      <button className={s.cloud_48}></button>
+      <button className={s.cloud_49}></button>
+      <button className={s.cloud_50}> </button>
+      <button className={s.cloud_51}></button>
+      <button className={s.cloud_52}></button>
+      <button className={s.cloud_53}> </button>
+    </section>
+
+  );
+};
+
+export default Neclace1;
